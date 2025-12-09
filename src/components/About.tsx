5956a5530382576ba9e2b121a2273afc056cc31b
Madson <madson.mms@gmail.com>
2025-12-09 17:25:10 -0300

    Inserting profile image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,9 @@
 import styles from '../assets/styles/About.module.css'
 import Skills from './Skills'
 import wave2 from '../assets/images/wave2.svg'
+import profile from '../assets/images/profile.jpg'
 
-export default function About(){
+export default function About() {
   const skills: [string, number][] = [
     ['Illustrator', 5],
     ['Photoshop', 4],
@@ -18,7 +19,9 @@ export default function About(){
     <section id="about" className={styles.about}>
       <img className={styles.wave} src={wave2} alt="" />
       <div className={styles.left}>
-        <div className={styles.profile} style={{ backgroundImage: 'url(/assets/profile.jpg)' }} aria-hidden="true"></div>
+        <div className={styles.profile} aria-hidden="true">
+          {/* <img src={profile} alt="Foto de Madson Martins" /> */}
+        </div>
       </div>
 
       <div className={styles.right}>
@@ -26,9 +29,10 @@ export default function About(){
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum vel mi vestibulum condimentum. Nunc convallis leo in orci blandit eleifend in vitae leo.
         </p>
 
-        <h2 className={styles.title}>Habilidades</h2>
-
-        <Skills items={skills} />
+        <div className={styles.habilities}>
+          <h2 className={styles.title}>Habilidades</h2>
+          <Skills items={skills} />
+        </div>
       </div>
     </section>
   )
